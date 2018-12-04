459dc65511ae6a6dbc7cef99c4e3e073bb536436
Mark J. Olah <markjolah@users.noreply.github.com>
2018-12-03 18:19:00 -0700

    Build system updates

--- ConfigureIPO.cmake
+++ ConfigureIPO.cmake
@@ -7,34 +7,13 @@
 # Find the TRNG (Tina's Random Number Generator Library)
 # GIT: https://github.com/rabauke/trng4.git
 # URL: https://www.numbercrunch.de/trng/
-find_path(TRNG_INCLUDE_DIR NAMES trng/lcg64.hpp HINTS ${TRNG_PREFIX_HINTS} PATH_SUFFIXES include)
-find_library(TRNG_LIBRARY NAMES trng4 HINTS ${TRNG_PREFIX_HINTS} PATH_SUFFIXES lib lib64)
+include(CheckIPOSupported)
+check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_SUPPORTED_ERROR)
 
-if(TRNG_INCLUDE_DIR)
-    file(READ ${TRNG_INCLUDE_DIR}/trng/config.hpp TRNG_CONFIG)
-    set(TRNG_VER_REGEX "TRNG_VERSION [0-9]+\\.[0-9]+")
-    string(REGEX MATCH ${TRNG_VER_REGEX} TRNG_VER_SUBSTR ${TRNG_CONFIG})
-    string(SUBSTRING ${TRNG_VER_SUBSTR} 13 -1 TRNG_VERSION_STRING)
+if( IPO_SUPPORTED )
+    message(STATUS "IPO / LTO enabled")
+    set_property(DIRECTORY PROPERTY INTERPROCEDURAL_OPTIMIZATION ON) 
+else()
+    message(STATUS "IPO / LTO not supported: <${IPO_SUPPORTED_ERROR}>")
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  TRNG
-  REQUIRED_VARS
-    TRNG_LIBRARY
-    TRNG_INCLUDE_DIR
-  VERSION_VAR
-    TRNG_VERSION_STRING
-)
-
-mark_as_advanced(TRNG_INCLUDE_DIR
-                 TRNG_LIBRARY
-                 TRNG_VERSION_STRING)
-
-if(TRNG_FOUND AND NOT TARGET TRNG::TRNG)
-    add_library(TRNG::TRNG UNKNOWN IMPORTED)
-    set_target_properties(TRNG::TRNG PROPERTIES
-        IMPORTED_LINK_INTERFACE_LANGUAGES CXX
-        IMPORTED_LOCATION ${TRNG_LIBRARY}
-        INTERFACE_INCLUDE_DIRECTORIES ${TRNG_INCLUDE_DIR})
-endif()
+# 
--- ConfigureNumericalDependenciesArmadillo.cmake
+++ ConfigureNumericalDependenciesArmadillo.cmake
@@ -7,12 +7,34 @@
 # Configure Armadillo, OpenMP, LAPACK, Blas, Pthreads for x-platform parallel numerical packages
 # with optional integration into matlab
 #
-include(CheckIPOSupported)
-check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_SUPPORTED_ERROR)
 
-if( IPO_SUPPORTED )
-    message(STATUS "IPO / LTO enabled")
-    set_property(DIRECTORY PROPERTY INTERPROCEDURAL_OPTIMIZATION ON) 
-else()
-    message(STATUS "IPO / LTO not supported: <${IPO_SUPPORTED_ERROR}>")
+#Enable Inter-proceedural optimization
+include(ConfigureIPO)
+
+# Armadillo
+find_package(Armadillo REQUIRED)
+add_definitions(-DARMA_USE_CXX11)
+add_definitions(-DARMA_DONT_USE_WRAPPER)
+add_definitions(-DARMA_BLAS_LONG)
+add_definitions(-DARMA_DONT_USE_OPENMP) #We want to control the use of openMP at a higher-grained level
+add_definitions(-DARMA_DONT_USE_HDF5)
+set_property(DIRECTORY APPEND PROPERTY COMPILE_DEFINITIONS $<$<CONFIG:Debug>:ARMA_PRINT_ERRORS>)
+set_property(DIRECTORY APPEND PROPERTY COMPILE_DEFINITIONS $<$<NOT:$<CONFIG:Debug>>:ARMA_NO_DEBUG>)
+if(OPT_EXTRA_DEBUG)
+    set_property(DIRECTORY APPEND PROPERTY COMPILE_DEFINITIONS $<$<CONFIG:Debug>:ARMA_EXTRA_DEBUG>)
 endif()
+
+# OpenMP
+find_package(OpenMP REQUIRED)
+
+# LAPACK & BLAS
+find_package(LAPACK REQUIRED)
+find_package(BLAS REQUIRED)
+
+# Pthreads
+if (WIN32)
+    find_library(PTHREAD_LIBRARY libwinpthread.dll REQUIRED)
+elseif(UNIX)
+    find_library(PTHREAD_LIBRARY libpthread.so REQUIRED)
+endif()
+#message(STATUS "Found Pthread Libarary: ${PTHREAD_LIBRARY}")
